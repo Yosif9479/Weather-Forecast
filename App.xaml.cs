3a9a35bd46eb80ab961da5452c393284f807ae4f
Joseph <ziyoyevyusuf@gmail.com>
2024-12-30 02:34:06 +0500

    Added env to app.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,20 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
+﻿using WeatherForecast.Tools;
 
 namespace WeatherForecast;
 
 public partial class App : Application
 {
-	public App()
+	private readonly Env _env;
+	
+	public App(Env env)
 	{
+		_env = env;
+		
 		InitializeComponent();
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new MainPage());
+		return new Window(new MainPage(_env));
 	}
 }
